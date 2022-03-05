6d190668c86dd77eb2b814505dc8a28bc7e6a04d
haneen <92938917+BatMan1683@users.noreply.github.com>
2022-03-05 22:22:18 +0000

    .

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("hi")
+#divide two digits 
+two_digit_number = input("Type a two digit number: ")
+value = int(two_digit_number [0]) + int(two_digit_number[1])
+print(two_digit_number [0]+ " + " + two_digit_number[1]+ " = "+ str(value))
