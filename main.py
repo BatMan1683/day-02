6d916062fd015c3bcc15d38315d837201d62bf0a
haneen <92938917+BatMan1683@users.noreply.github.com>
2022-03-09 10:10:21 +0000

    .

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 #divide two digits 
-two_digit_number = input("Type a two digit number: ")
-value = int(two_digit_number [0]) + int(two_digit_number[1])
-print(two_digit_number [0]+ " + " + two_digit_number[1]+ " = "+ str(value))
+# two_digit_number = input("Type a two digit number: ")
+# value = int(two_digit_number [0]) + int(two_digit_number[1])
+# print(two_digit_number [0]+ " + " + two_digit_number[1]+ " = "+ str(value))
+#BMI calculator
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+print(float(weight) / (float(height) * float(height)))
