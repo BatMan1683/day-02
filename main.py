417282d6a8f31ca8314fd300c504f3dea628ef2b
haneen <92938917+BatMan1683@users.noreply.github.com>
2022-03-09 12:18:06 +0000

    .

--- main.py
+++ main.py
@@ -3,6 +3,17 @@
 # value = int(two_digit_number [0]) + int(two_digit_number[1])
 # print(two_digit_number [0]+ " + " + two_digit_number[1]+ " = "+ str(value))
 #BMI calculator
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-print(int(float(weight) / (float(height) * float(height))))
+# 
+# 365 days in a year, 52 weeks in a year and 12 months in a year
+age = input("What is your current age?")
+agey = 90 - int(age) 
+years = agey * 365
+week = agey * 52
+month = agey * 12
+print(f" you have {years} days, {week} weeks, and {month} months left.")
+
+
+
+
+
+
